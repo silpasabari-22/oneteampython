7b3774971b42492c87ab793ba09b5adbdd7e4e34
silpa <silpasabari2203@gmail.com>
2025-06-12 09:52:22 +0530

    initial commit

---
+++ swap2.py
@@ -0,0 +1,21 @@
+# a=10
+# b=20
+
+# b=a+b
+# a=b-a
+# b=b-a
+# print(a)
+# print(b)
+
+
+# a=10
+# b=20
+# a,b=b,a
+# print("a=",a,"b=",b)
+# print("a={} b={}".format(a,b))
+# print(f"a={a} b={b}")
+
+
+a="silpa"
+print(type(a))
+
