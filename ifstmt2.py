753be1c15cc85afe7f48e39c3772eb59d4b33e54
silpa <silpasabari2203@gmail.com>
2025-06-17 19:34:47 +0530

    second

---
+++ ifstmt2.py
@@ -0,0 +1,40 @@
+#IF STATEMENT
+
+# a=float(input("Enter a number :"))
+# if a>0:
+#     print("The number is positive")
+
+
+# a=int(input("Enter a number :"))
+# if a%5==0:
+#     print("The number is divisible by 5")
+
+
+# vowel=input("Enter a character ")
+# if(vowel in ['a','e','i','o','u']):
+#     print("This is a VOWEL")
+
+
+
+
+#IF ELSE STATEMENT
+
+# a=int(input("Enter a number "))
+# if a%2==0:
+#     print("The number is Even")
+# else:
+#     print("The number is Odd")
+
+
+# age=int(input("Enter your age "))
+# if age>=18:
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible to vote")
+
+
+
+
+
+
+
