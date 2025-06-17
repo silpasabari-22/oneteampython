fd23b9aca9e42c9bb35314908b15dc8e569f8a36
silpa <silpasabari2203@gmail.com>
2025-06-17 10:35:16 +0530

    second

---
+++ ifstmt.py
@@ -0,0 +1,17 @@
+# i=4
+# if(i % 2 == 0):
+#    print("The number is even")
+# print("not in if")
+
+
+# age = 20
+# if age >= 18:
+#     print("eligible to vote")
+
+
+ number = 10
+ if number > 0:
+     print("The number is positive.")
+
+   
+
